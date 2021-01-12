120982a827f4321cb590af1ae573b95c2da55dac
arata <arata04962@gmail.com>
2021-01-13 06:39:14 +0900

    [update] remove exit button

--- FindTheEmoji/include/Scenes/Title.hpp
+++ FindTheEmoji/include/Scenes/Title.hpp
@@ -7,9 +7,6 @@ private:
 	const Rect m_startButton = Rect(Arg::center = Scene::Center().movedBy(0, 0), 300, 60);
 	Transition m_startTransition = Transition(0.4s, 0.2s);
 
-	const Rect m_exitButton = Rect(Arg::center = Scene::Center().movedBy(0, 100), 300, 60);
-	Transition m_exitTransition = Transition(0.4s, 0.2s);
-
 	const Rect screenRect = Rect(0, 0, Scene::Width(), Scene::Height());
 	Array<Chara> charas;
 
--- FindTheEmoji/src/Scenes/Title.cpp
+++ FindTheEmoji/src/Scenes/Title.cpp
@@ -20,17 +20,12 @@ Title::Title(const InitData& init) : IScene(init) {
 
 void Title::update() {
 	m_startTransition.update(m_startButton.mouseOver());
-	m_exitTransition.update(m_exitButton.mouseOver());
-	if (m_startButton.mouseOver() || m_exitButton.mouseOver()) {
+	if (m_startButton.mouseOver()) {
 		Cursor::RequestStyle(CursorStyle::Hand);
 	}
 
 	if (m_startButton.leftClicked()) {
 		changeScene(State::Game, SceneTrasisionTime);
-	}
-
-	if (m_exitButton.leftClicked()) {
-		System::Exit();
 	}
 	
 	for (auto& chara : charas) {
@@ -58,9 +53,7 @@ void Title::draw() const {
 
 	// ボタン
 	m_startButton.draw(ColorF(1.0, m_startTransition.value())).drawFrame(2);
-	m_exitButton.draw(ColorF(1.0, m_exitTransition.value())).drawFrame(2);
 	FontAsset(U"Menu")(U"Start").drawAt(m_startButton.center(), ColorF(0.25));
-	FontAsset(U"Menu")(U"Exit").drawAt(m_exitButton.center(), ColorF(0.25));
 
 	// 背景
 	Rect(0, 500, Scene::Width(), Scene::Height() - 500)
